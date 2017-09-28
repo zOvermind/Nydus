b74bac04382e2c216d021d13c361202dcf95821f
Peter <fabianpeter85@gmail.com>
2017-09-28 15:40:12 +0200

    Example

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-const express = require('express')
-const app = express()
-
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
-
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
-})
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
