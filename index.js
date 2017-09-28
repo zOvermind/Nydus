3a455f02519bdbfc148b59cca233e7807c3cc770
Peter <fabianpeter85@gmail.com>
2017-09-28 15:49:29 +0200

    Example

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
