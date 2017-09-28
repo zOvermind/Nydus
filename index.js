618e7574ba2f163f3ec5ac20b02cff103ebfcabe
Peter <fabianpeter85@gmail.com>
2017-09-28 15:47:21 +0200

    Example

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
- 
-server.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", port " + server_port )
-});
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(server_port, server_ip_address);
+
+
+console.log('Server running');
